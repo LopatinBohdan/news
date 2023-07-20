eb37dc9b4b46934f0b1706c8c6c817188e561c6c
Monseratti <dnamaxy@gmail.com>
2023-07-20 18:00:14 +0300

    commit 20_07_23_18_00

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,15 +16,17 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 
 
     <!-- FontAwesome 6.2.0 CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
         integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
+
     <!-- (Optional) Use CSS or JS implementation -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/js/all.min.js"
         integrity="sha512-naukR7I+Nk6gp7p5TMA4ycgfxaZBJ7MO5iC3Fp6ySQyKFHOGfpkSZkYVWV5R7u7cfAicxanwYQ5D1e17EfJcMA=="
@@ -31,34 +34,39 @@
 
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
     <style>
-      main {
-        display: flex;
-        flex-direction: column;
-        min-height: 76vh;
-      }
-      .container {
-        flex: 1;
-      }
-      .myNav{
-        position: fixed;
-        top: 0;
-        width: 100%;
-        background-color: #333;
-      }
-      .footer {
-        height: 100%;
-        background-color: #000;
-        text-align: center;
-        width: 100%;
-        position: sticky;
-        bottom: 0;
-      }
+        main {
+            display: flex;
+            flex-direction: column;
+            min-height: 76vh;
+        }
+
+        .container {
+            flex: 1;
+        }
+
+        .myNav {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            background-color: #333;
+        }
+
+        .footer {
+            height: 100%;
+            background-color: #000;
+            text-align: center;
+            width: 100%;
+            position: sticky;
+            bottom: 0;
+        }
     </style>
     @yield('styles')
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body style="background-color: #e6edf3; 
+
+<body
+    style="background-color: #e6edf3; 
 font-family: -apple-system,BlinkMacSystemFont,'Segoe UI','Noto Sans',Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji'; 
 font-size: var(--body-font-size, 14px);
 line-height: 1.5; color: #0d1117">
@@ -67,60 +75,53 @@ line-height: 1.5; color: #0d1117">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{-- {{ config('app.name', 'Laravel') }} --}}
-                    Apartment is here!
+                    <i class="fa-solid fa-house-tsunami fa-fade fa-xl" style="color: #2014f8;"></i>
+                    <i>Apartment is here!</i>
                 </a>
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                   @can('role administrate')
-                    <li class="nav-item">
-                        <a href={{URL::to('/roles')}} class="nav-link">To Roles</a>
-                    </li>
-                    @endcan
-                    @can('Full access')
-                    <li class="nav-item">
-                        <a href={{URL::to('/users')}} class="nav-link">To Users</a>
-                    </li>
-                    @endcan
-                    @can('permission administrate')
-                    <li class="nav-item">
-                        <a href={{URL::to('/permissions')}} class="nav-link">To Permissions</a>
-                    </li>
-                    @endcan
-                    @can('status administrate')
-                    <li class="nav-item">
-                        <a href={{URL::to('/statuses')}} class="nav-link">To Statuses</a>
-                    </li>
-                    @endcan
-                    @can('placement administrate')
-                    <li class="nav-item">
-                        <a href={{URL::to('/placements')}} class="nav-link">To Placements</a>
-                    </li>
-                    @endcan
-                    @can('Full access')
-                    <li class="nav-item">
-                        <a href={{URL::to('/categories')}} class="nav-link">To Categories</a>
-                    </li>
-                    @endcan
-                    @can('Full access')
-                    <li class="nav-item">
-                        <a href={{URL::to('/comforts')}} class="nav-link">To Comforts</a>
-                    </li>
-                    @endcan
-                </ul>
-                
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
+                    <ul class="navbar-nav">
+                        @can('role administrate')
+                            <li class="nav-item">
+                                <a href={{ URL::to('/roles') }} class="nav-link">To Roles</a>
+                            </li>
+                        @endcan
+                        @can('permission administrate')
+                            <li class="nav-item">
+                                <a href={{ URL::to('/permissions') }} class="nav-link">To Permissions</a>
+                            </li>
+                        @endcan
+                        @can('status administrate')
+                            <li class="nav-item">
+                                <a href={{ URL::to('/statuses') }} class="nav-link">To Statuses</a>
+                            </li>
+                        @endcan
+                        @can('placement administrate')
+                            <li class="nav-item">
+                                <a href={{ URL::to('/placements') }} class="nav-link">To Placements</a>
+                            </li>
+                        @endcan
+                        @can('Full access')
+                            <li class="nav-item">
+                                <a href={{ URL::to('/users') }} class="nav-link">To Users</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href={{ URL::to('/categories') }} class="nav-link">To Categories</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href={{ URL::to('/comforts') }} class="nav-link">To Comforts</a>
+                            </li>
+                        @endcan
                     </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
-                        
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
@@ -135,7 +136,7 @@ line-height: 1.5; color: #0d1117">
                             @endif
                         @else
                             <li class="nav-item">
-                                <a class="nav-link" href="{{route('orders.create')}}">
+                                <a class="nav-link" href="{{ route('orders.create') }}">
                                     @if (isset($_COOKIE[Auth::user()->id]))
                                         <i class="fa-solid fa-cart-arrow-down fa-beat fa-lg"></i>
                                     @else
@@ -144,15 +145,16 @@ line-height: 1.5; color: #0d1117">
                                 </a>
                             </li>
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a href={{URL::to('/orders')}} class="dropdown-item">My orders</a>
+                                    <a href={{ URL::to('/orders') }} class="dropdown-item">My orders</a>
                                     <hr>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
@@ -171,93 +173,88 @@ line-height: 1.5; color: #0d1117">
         <main class="d-flex flex-column p-3">
             @yield('content')
         </main>
+        <!-- Footer -->
+        <footer class="footer bg-gradient text-center text-white position-sticky">
+            <!-- Grid container -->
+            <div class="container p-3">
+                <!-- Section: Social media -->
+                <section class="mb-2">
+                    <!-- Facebook -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                            class="fab fa-facebook-f"></i></a>
 
-          <!-- Footer -->
-<footer class="footer bg-gradient text-center text-white position-sticky">
-    <!-- Grid container -->
-    <div class="container p-3">
-      <!-- Section: Social media -->
-      <section class="mb-2">
-        <!-- Facebook -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-          ><i class="fab fa-facebook-f"></i
-        ></a>
-  
-        <!-- Twitter -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-          ><i class="fab fa-twitter"></i
-        ></a>
-  
-        <!-- Google -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-          ><i class="fab fa-google"></i
-        ></a>
-  
-        <!-- Instagram -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-          ><i class="fab fa-instagram"></i
-        ></a>
-  
-        <!-- Linkedin -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-          ><i class="fab fa-linkedin-in"></i
-        ></a>
-  
-        <!-- Github -->
-        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"
-          ><i class="fab fa-github"></i
-        ></a>
-      </section>
-      <!-- Section: Social media -->
-  
-      <!-- Section: Form -->
-      <section class="">
-        <form action="">
-          <!--Grid row-->
-          <div class="row d-flex justify-content-center">
-            <!--Grid column-->
-            <div class="col-auto">
-              <p class="pt-2">
-                <strong>Sign up for our newsletter</strong>
-              </p>
+                    <!-- Twitter -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                            class="fab fa-twitter"></i></a>
+
+                    <!-- Google -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                            class="fab fa-google"></i></a>
+
+                    <!-- Instagram -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                            class="fab fa-instagram"></i></a>
+
+                    <!-- Linkedin -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                            class="fab fa-linkedin-in"></i></a>
+
+                    <!-- Github -->
+                    <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button"><i
+                            class="fab fa-github"></i></a>
+                </section>
+                <!-- Section: Social media -->
+
+                <!-- Section: Form -->
+                <section class="">
+                    <form action="">
+                        <!--Grid row-->
+                        <div class="row d-flex justify-content-center">
+                            <!--Grid column-->
+                            <div class="col-auto">
+                                <p class="pt-2">
+                                    <strong>Sign up for our newsletter</strong>
+                                </p>
+                            </div>
+                            <!--Grid column-->
+
+                            <!--Grid column-->
+                            <div class="col-md-5 col-12">
+                                <!-- Email input -->
+                                <div class="form-outline form-white mb-2">
+                                    <input type="email" id="form5Example21" class="form-control"
+                                        placeholder="Email address" />
+                                </div>
+                            </div>
+                            <!--Grid column-->
+
+                            <!--Grid column-->
+                            <div class="col-auto">
+                                <!-- Submit button -->
+                                <button type="submit" class="btn btn-outline-light mb-2">
+                                    Subscribe
+                                </button>
+                            </div>
+                            <!--Grid column-->
+                        </div>
+                        <!--Grid row-->
+                    </form>
+                </section>
             </div>
-            <!--Grid column-->
-  
-            <!--Grid column-->
-            <div class="col-md-5 col-12">
-              <!-- Email input -->
-              <div class="form-outline form-white mb-2">
-                <input type="email" id="form5Example21" class="form-control" placeholder="Email address" />
-              </div>
+            <!-- Grid container -->
+
+            <!-- Copyright -->
+            <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.2);">
+                © 2023 Copyright:
+                <a class="text-white" href="/">Booking KR</a>
             </div>
-            <!--Grid column-->
-  
-            <!--Grid column-->
-            <div class="col-auto">
-              <!-- Submit button -->
-              <button type="submit" class="btn btn-outline-light mb-2">
-                Subscribe
-              </button>
-            </div>
-            <!--Grid column-->
-          </div>
-          <!--Grid row-->
-        </form>
-      </section>
-    </div>
-    <!-- Grid container -->
-  
-    <!-- Copyright -->
-    <div class="text-center p-2" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2023 Copyright:
-      <a class="text-white" href="/">Booking KR</a>
-    </div>
-    <!-- Copyright -->
-  </footer>
-  <!-- Footer -->
+            <!-- Copyright -->
+        </footer>
+        <!-- Footer -->
     </div>
 
     @yield('script')
 
 </body>
+
 </html>
